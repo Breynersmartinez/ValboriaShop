960af0e5242c7ea30b3b9e66391288df4f098256
Breiner Saul Martinez Muñoz <153401516+Breynersmartinez@users.noreply.github.com>
2024-11-19 00:52:59 -0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -3,15 +3,20 @@ function changeImage(element) {
     document.getElementById('main-image').src = element.src;
 }
 
-function showOrderForm() {
+function showOrderForm1() {
     document.getElementById('order-form').classList.remove('hidden');
 }
-
+function showOrderForm2() {
+    document.getElementById('order-form').classList.remove('hidden');
+}
+function showOrderForm3() {
+    document.getElementById('order-form').classList.remove('hidden');
+}
 function closeOrderForm() { 
     document.getElementById('order-form').classList.add('hidden');
 }
 
-function sendOrder() {
+function sendOrder1() {
     const name = document.getElementById('name').value;
     const phone = document.getElementById('phone').value;
     const email = document.getElementById('email').value;
@@ -32,6 +37,52 @@ Celular del cliente: ${phone}.
 Ciudad de destino: ${city}.
 Dirección: ${address}.
 Talla y color: ${confirmSize} / ${confirmColor}.`;
+
+    /**/
+    function sendOrder2() {
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+    const email = document.getElementById('email').value;
+    const department = document.getElementById('department').value;
+    const city = document.getElementById('city').value;
+    const address = document.getElementById('address').value;
+    const neighborhood = document.getElementById('neighborhood').value;
+    const confirmSize = document.getElementById('confirm-size').value;
+    const confirmColor = document.getElementById('confirm-color').value;
+    const color = document.getElementById('color').value;
+    const size = document.getElementById('size').value;
+    const price = "$25000";
+
+    const message = `Saludos, mi nombre es: ${name}.
+Acabo de hacer un pedido del producto - ${color} / ${size} por un total de: ${price}.
+Pago:
+Celular del cliente: ${phone}.
+Ciudad de destino: ${city}.
+Dirección: ${address}.
+Talla y color: ${confirmSize} / ${confirmColor}.`;
+        /**/
+        function sendOrder3() {
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+    const email = document.getElementById('email').value;
+    const department = document.getElementById('department').value;
+    const city = document.getElementById('city').value;
+    const address = document.getElementById('address').value;
+    const neighborhood = document.getElementById('neighborhood').value;
+    const confirmSize = document.getElementById('confirm-size').value;
+    const confirmColor = document.getElementById('confirm-color').value;
+    const color = document.getElementById('color').value;
+    const size = document.getElementById('size').value;
+    const price = "$25000";
+
+    const message = `Saludos, mi nombre es: ${name}.
+Acabo de hacer un pedido del producto - ${color} / ${size} por un total de: ${price}.
+Pago:
+Celular del cliente: ${phone}.
+Ciudad de destino: ${city}.
+Dirección: ${address}.
+Talla y color: ${confirmSize} / ${confirmColor}.`;
+            /**/
 
     const whatsappUrl = `https://wa.me/573242367080?text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, '_blank');
